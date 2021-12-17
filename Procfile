3881584372a1422556915eceab7087a0e64e70a4
JohannSuarez <johann.suarez92@gmail.com>
2021-12-17 09:21:42 -0800

    Preparing for Heroku deployment.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node --inspect dist/http.js
---
+++ src/wrapper.ts
@@ -0,0 +1,20 @@
+let GunW = class {
+  /*
+    Gun wrapper, simplifies creating, reading, writing,
+    and updating data. Because every operation involves chaining .get()
+    and .put()
+
+    gun.get('something').get('not').get('exist').get('yet').put("Hello World!");
+     `.put` will if needed, backwards create a document
+     so "Hello World!" has a place to be saved.
+  */
+
+  constructor(peers: Array<string>) {
+    /*
+    It 
+
+    */
+
+  }
+
+}
